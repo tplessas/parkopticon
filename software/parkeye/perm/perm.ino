804b2bdd01fba0469e9d4b32962140208fe3c4cc
tplessas <teopless@gmail.com>
2020-04-26 18:38:38 +0300

    added comments to perm

--- software/parkeye/perm/perm.ino
+++ software/parkeye/perm/perm.ino
@@ -1,21 +1,31 @@
 // ParkEye Runtime with Messaging (PERM)
 // Created by Theodoros Plessas (8160192) for Artifex Electronics
 
+//libraries
 #include <SPI.h>
 #include <MFRC522.h>
 #include <HCSR04.h>
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
 
+//initializing sensors
 HCSR04 hc(2, 3);
 MFRC522 rfid(10, 9);
 MFRC522::MIFARE_Key key;
 LiquidCrystal lcd(8, A0, 4, 5, 6, 7);
 
-String uid;
+//config variables
+String uid; //ParkEye UID
 int opmode;
-String rfuid;
+String rfuid; //RFID tag UID, always loaded no matter the opmode
 
+//status variables
+bool occ = false;
+int cycles = 0; //cycles ran up to selfdg
+int sr04 = 0; //inaccurate reading count
+bool infract = false; //infraction status, used in rfid subroutine
+
+//display graphics as custom characters
 byte logo[8] =
 {
   0b01110,
@@ -76,27 +86,26 @@ byte bad[8] =
   0b00100
 };
 
-bool occ = false;
-int cycles = 0;
-int sr04 = 0;
-bool infract = false;
-
 void setup() {
   Serial.begin(9600);
 
+  //reading and printing uid
   char temp[6];
-  temp[5] = 0x00;
+  temp[5] = 0x00; //null char, end of byte array
   for (int i = 0; i < 5; i++) {
     temp[i] = EEPROM.read(i);
   }
   uid = String(temp);
   Serial.print(uid);
 
+  //reading and printing opmode
   opmode = EEPROM.read(5);
   Serial.print(opmode);
 
+  //reading and printing rfuid
+  //TODO find out what it does when all bytes==255
   char temp2[12];
-  temp2[11] = 0x00;
+  temp2[11] = 0x00; //null char, end of byte array
   for (int i = 0; i < 11; i++) {
     temp2[i] = EEPROM.read(i + 6);
   }
@@ -104,6 +113,8 @@ void setup() {
   Serial.print(rfuid);
   Serial.print("*");
 
+  //opmode 1, 2: init SPI bus for MFRC522 and setting key to default
+  //TODO store key in config?
   if (opmode != 0) {
     SPI.begin();
     rfid.PCD_Init();
@@ -112,6 +123,7 @@ void setup() {
     }
   }
 
+  //init LCD, creating symbols and displaying boot message for 1500ms
   lcd.begin(16, 2);
   lcd.createChar(0, logo);
   lcd.createChar(1, idsym);
@@ -126,50 +138,61 @@ void setup() {
   lcd.print("Artifex/Tronics");
 
   delay(1500);
+
+  //write uid and parking rights to display
+  //TODO communicate with ParkBrain to print limited when opmode==0
+  lcd.clear();
+  lcd.setCursor(10, 0);
+  lcd.write(byte(1));
+  lcd.print(uid);
+  if (opmode == 0) {
+    lcd.setCursor(0, 1);
+    lcd.print("Free Pkng");
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.print("Limited Pkng");
+  }
 }
 
 void loop() {
 
+  //update display at ParkEye state change
   if (!infract && !occ) {
-    lcd.clear();
+    lcd.setCursor(0, 0);
     lcd.write(byte(0));
-    lcd.setCursor(10, 0);
-    lcd.write(byte(1));
-    lcd.print(uid);
-
-    if (opmode == 0) {
-      lcd.setCursor(0, 1);
-      lcd.print("Free Pkng");
-    } else {
-      lcd.setCursor(0, 1);
-      lcd.print("Limited Pkng");
-    }
+    lcd.print("         "); //previous message whitespaced
   }
 
-  int dist = hc.dist();
-  check(dist);
+  //distance check from vehicle
+  check((int)hc.dist());
 
   if (cycles == 1000) {
     selfdg();
     cycles = 0;
   }
+
+  //delay before next check
   delay(500);
 }
 
+//compare vehicle dist to cutoffs
 void check(int dist) {
-  if ((dist < 3) && (occ == false)) {
+  if ((dist < 3) && (occ == false)) { //TODO store cutoff in config?
     occupy();
   }
-  else if ((dist > 10) && (occ == true)) {
+  else if ((dist > 10) && (occ == true)) { //10 as cutoff to avoid state change due to possible imprecise measurement
     freed();
   }
   cycles++;
 }
 
+//occupy parking spot
 void occupy() {
   occ = true;
   Serial.print("OCCD*");
 
+  //opmode 1,2: rfid subroutine
+  //TODO add delays between occupy and check, reread attempts to ensure successful reading
   if (opmode != 0) {
     int present = 0;
     int read = 0;
@@ -228,6 +251,7 @@ void occupy() {
         delay(1000);
       } else {
         present++;
+        //NOCARD if tag not found this number of times
         if (present == 2) {
           Serial.print("NOCARD*");
           infract = true;
@@ -240,6 +264,7 @@ void occupy() {
   }
 }
 
+//free parking spot
 void freed() {
   occ = false;
   Serial.print("FREE*");
@@ -248,9 +273,11 @@ void freed() {
   }
 }
 
+//HC-SRO4 self-diagnostic, takes 10 readings and checks for significant delta from previous one
+//TODO make more reliable, adjust sensitivity
 void selfdg() {
   int dists[10];
-  delay(10);
+  delay(10); //minimum delay to ensure accurate readings
 
   for (int i = 0; i < 10; i++) {
     dists[i] = hc.dist();
@@ -261,7 +288,9 @@ void selfdg() {
       sr04++;
     }
   }
-  if (sr04 > 20) {
+
+  //fail if it happens over 5 times in device uptime
+  if (sr04 > 10) {
     Serial.print("SR04FAIL*");
     lcd.clear();
     lcd.setCursor(1, 0);
@@ -275,6 +304,9 @@ void selfdg() {
   }
 }
 
+//turns RFID byte array into spaced hex string
+//TODO make all rfid operations use bytes, not ascii byte-to-string nonsense
+//this is dumb
 String castString() {
   String str;
 
